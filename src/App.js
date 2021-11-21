9d934fbb2cd7f1bcbceed336b60384d68593fef3
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-11-21 14:43:38 +0200

    app added data and props to gallery

--- src/App.js
+++ src/App.js
@@ -1,7 +1,88 @@
 import './App.css';
-
+import HotelsGallery from './component/HotelsGallery';
 function App() {
-  return <div className="App"></div>;
+  const hotels = [
+    { name: 'מלון מרום', street: 'פלמח', house: '051', phone: '8254355' },
+    {
+      name: 'אלישע בעמ',
+      street: 'כץ יאיר',
+      house: '012',
+      phone: '8309009',
+    },
+    {
+      name: 'אירוח הר הכרמל',
+      street: 'דרך הים',
+      house: '103',
+      phone: '8381414',
+    },
+    {
+      name: 'מלון הר הכרמל',
+      street: 'שושנת הכרמל',
+      house: '067',
+      phone: '8381414',
+    },
+    {
+      name: 'קראון פלזה חיפה',
+      street: 'יפה נוף',
+      house: '111',
+      phone: '8350800',
+    },
+    {
+      name: 'מלון דביר',
+      street: 'יפה נוף',
+      house: '124',
+      phone: '8389139',
+    },
+    {
+      name: 'מלון נוף',
+      street: 'שדרות הנשיא',
+      house: '101',
+      phone: '8354311',
+    },
+    {
+      name: 'מלון דן פנורמה',
+      street: 'שדרות הנשיא',
+      house: '105',
+      phone: '8334510',
+    },
+    {
+      name: 'בית שלום כרמל',
+      street: 'שדרות הנשיא',
+      house: '110',
+      phone: '8377481',
+    },
+    {
+      name: 'מלון יערות הכרמל',
+      street: 'יערות הכרמל',
+      house: '31900',
+      phone: '',
+    },
+    {
+      name: 'מלון וילה כרמל',
+      street: 'היינריך היינה',
+      house: '1',
+      phone: '8375777/8',
+    },
+    {
+      name: 'מלון גלרי חיפה',
+      street: 'הרצל',
+      house: '061',
+      phone: '8616161',
+    },
+    {
+      name: 'מלון ליאונרדו',
+      street: 'דוד אלעזר',
+      house: '10',
+      phone: '8508888',
+    },
+  ];
+
+  return (
+    <div className="App">
+      <h1>Hotel App</h1>
+      <HotelsGallery hotels={hotels} />
+    </div>
+  );
 }
 
 export default App;
